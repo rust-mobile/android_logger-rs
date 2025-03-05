2581c258b996135dc6aa44f28ca831f198d2b5ec
Marcin Radomski <dextero@google.com>
2025-03-05 10:39:19 +0000

    Reformat with 1.85 toolchain

--- src/platform_log_writer.rs
+++ src/platform_log_writer.rs
@@ -1,5 +1,5 @@
 use crate::arrays::slice_assume_init_ref;
-use crate::{android_log, uninit_array, LogId, LOGGING_MSG_MAX_LEN};
+use crate::{LOGGING_MSG_MAX_LEN, LogId, android_log, uninit_array};
 use log::Level;
 #[cfg(target_os = "android")]
 use log_ffi::LogPriority;
@@ -153,11 +153,7 @@ impl fmt::Write for PlatformLogWriter<'_> {
                 .enumerate()
                 .fold(None, |acc, (i, (output, input))| {
                     output.write(*input);
-                    if *input == b'\n' {
-                        Some(i)
-                    } else {
-                        acc
-                    }
+                    if *input == b'\n' { Some(i) } else { acc }
                 });
 
             // update last \n index
